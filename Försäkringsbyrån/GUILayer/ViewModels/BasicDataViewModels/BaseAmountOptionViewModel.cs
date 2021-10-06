c2731d7407e1522c3cb87c55efc2810fe2630002
Fanny Berggren <s194840@student.hb.se>
2021-10-06 13:28:50 +0200

    BaseAmountOption

---
+++ Försäkringsbyrån/GUILayer/ViewModels/BasicDataViewModels/BaseAmountOptionViewModel.cs
@@ -0,0 +1,45 @@
+﻿using GUILayer.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace GUILayer.ViewModels.BasicDataViewModels
+{
+    public class BaseAmountOptionViewModel : BaseViewModel
+    {
+        public static readonly BaseAmountOptionViewModel Instance = new BaseAmountOptionViewModel();
+
+        private BaseAmountOptionViewModel()
+        {
+
+        }
+
+        private ICommand _addBtn;
+        public ICommand AddBaseAmountOption_Btn
+        {
+            get => _addBtn ?? (_addBtn = new RelayCommand(x => { AddBaseAmountOption(); CanCreate(); }));
+        }
+
+        public bool CanCreate() => true;
+
+        public static void AddBaseAmountOption()
+        {
+            // Code to actualy add baseamount to the database.
+        }
+
+        private ICommand remove_Btn;
+        public ICommand RemoveBaseAmountOption_Btn
+        {
+            get => remove_Btn ?? (remove_Btn = new RelayCommand(x => { RemoveBaseAmountOption(); CanCreate(); }));
+        }
+
+        public static void RemoveBaseAmountOption()
+        {
+            // Code to actualy add baseamount to the database.
+        }
+
+    }
+}
